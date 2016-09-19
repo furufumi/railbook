59235b29410125c570e8fc43af78ce4f5641f4ac
furukawa fumika <furukawa_fumika@outlook.jp>
2016-09-20 00:30:56 +0900

    ログアウト追加

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,7 @@
 ﻿class LoginController < ApplicationController
   skip_before_action :check_logined
   def index
+    render :index, layout: false
   end
 
 
@@ -9,7 +10,8 @@
     if usr then
       reset_session
       session[:usr] = usr.id
-      redirect_to params[:referer]
+      session[:loger] = usr.userid
+      redirect_to books_path
     else
       flash.now[:referer] = params[:referer]
       @error = 'ユーザーID/パスワードが間違っています。'
